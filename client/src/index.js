e2230d12065e569e901241e7d97431387ca537b8
Ahmed <ahmedali.96@hotmail.com>
2020-08-11 11:33:08 -0400

    auth state created

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
+import setAuthToken from "./utils/setAuthToken";
 
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 ReactDOM.render(<App />, document.getElementById("root"));
