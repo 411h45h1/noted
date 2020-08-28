e55dca74e8fc97b50ca4455c7ceb6f875ce4d376
Ahmed <ahmedali.96@hotmail.com>
2020-08-27 21:10:45 -0400

    refactored app back to base & connected firebase

--- client/src/components/index.js
+++
@@ -1,6 +0,0 @@
-//auth
-export { default as Login } from "./auth/Login";
-export { default as Register } from "./auth/Register";
-//components
-export { default as Onboard } from "./onboard/Onboard";
-export { default as Notes } from "./notes/Notes";
--- client/src/context/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AppState } from "./app/AppState";
-export { default as AuthState } from "./auth/AuthState";
